b66ab573ea73ad2d10b4abaaf69d3959740f6ed1
Odunayo Obajuluwa <aobajuluwa@salesforce.com>
2019-02-07 14:38:54 -0600

    first commit ever

---
+++ js/main.js
@@ -0,0 +1,57 @@
+function showHide() {
+  document.getElementsByClassName("lighting_menu")[0].classList.toggle("show");
+}
+
+function slideLeft() {
+  document.getElementsByClassName("lamps_menu")[0].classList.add("slideLeft");
+  var height1 = document.getElementsByClassName("lamps_menu")[0].clientHeight;
+  document.getElementsByClassName("lighting_menu_wrap")[0].style.height = height1 + "px";
+
+}
+function slideRight() {
+  document.getElementsByClassName("lamps_menu")[0].classList.remove("slideLeft");
+  var height1 = document.getElementsByClassName("lighting_menu")[0].clientHeight;
+  document.getElementsByClassName("lighting_menu_wrap")[0].style.height = height1 + "px";
+}
+
+function slidePendantsLeft() {
+	document.getElementsByClassName("pendants_menu")[0].classList.add("slideLeft");
+	var height1 = document.getElementsByClassName("pendants_menu")[0].clientHeight;
+  	document.getElementsByClassName("lighting_menu_wrap")[0].style.height = height1 + "px";
+}
+function slidePendantsRight() {
+	document.getElementsByClassName("pendants_menu")[0].classList.remove("slideLeft");
+	var height1 = document.getElementsByClassName("lighting_menu")[0].clientHeight;
+  	document.getElementsByClassName("lighting_menu_wrap")[0].style.height = height1 + "px";
+}
+
+function slideItemLeft() {
+	document.getElementsByClassName("item_7_menu")[0].classList.add("slideLeft");
+	var height1 = document.getElementsByClassName("item_7_menu")[0].clientHeight;
+  	document.getElementsByClassName("lighting_menu_wrap")[0].style.height = height1 + "px";
+}
+function slideItemRight() {
+	document.getElementsByClassName("item_7_menu")[0].classList.remove("slideLeft");
+	var height1 = document.getElementsByClassName("lamps_menu")[0].clientHeight;
+  	document.getElementsByClassName("lighting_menu_wrap")[0].style.height = height1 + "px";
+}
+
+function slideIndustrialLeft() {
+	document.getElementsByClassName("industrial_menu")[0].classList.add("slideLeft");
+	var height1 = document.getElementsByClassName("industrial_menu")[0].clientHeight;
+  	document.getElementsByClassName("lighting_menu_wrap")[0].style.height = height1 + "px";
+}
+function slideIndustrialRight() {
+	document.getElementsByClassName("industrial_menu")[0].classList.remove("slideLeft");
+	var height1 = document.getElementsByClassName("pendants_menu")[0].clientHeight;
+  document.getElementsByClassName("lighting_menu_wrap")[0].style.height = height1 + "px";
+}
+
+window.onclick = function(e) {
+  if (!e.target.closest('.lighting')) {
+  	var myMenu = document.getElementsByClassName("lighting_menu");
+    	if (myMenu[0].classList.contains('show')) {
+      		myMenu[0].classList.remove('show');
+    	}
+  	}
+}
