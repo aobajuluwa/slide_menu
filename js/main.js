41b9682ad98cb08bfef289c92d23ac29c1dfd565
Odunayo Obajuluwa <aobajuluwa@salesforce.com>
2019-02-07 15:03:51 -0600

    reaaally made menu reset when clicked out of..i hope lol

--- js/main.js
+++ js/main.js
@@ -1,5 +1,11 @@
 function showHide() {
   document.getElementsByClassName("lighting_menu")[0].classList.toggle("show");
+    if (document.getElementsByClassName("lighting_menu")[0].classList.contains('show')) {
+        document.getElementsByClassName("lamps_menu")[0].classList.remove("slideLeft");
+        document.getElementsByClassName("pendants_menu")[0].classList.remove("slideLeft");
+        document.getElementsByClassName("item_7_menu")[0].classList.remove("slideLeft");
+        document.getElementsByClassName("industrial_menu")[0].classList.remove("slideLeft");
+    }
 }
 
 function slideLeft() {
@@ -51,11 +57,11 @@ window.onclick = function(e) {
   if (!e.target.closest('.lighting')) {
   	var myMenu = document.getElementsByClassName("lighting_menu");
     	if (myMenu[0].classList.contains('show')) {
-      		myMenu[0].classList.remove('show');
-          document.getElementsByClassName("lamps_menu")[0].classList.remove("slideLeft");
-          document.getElementsByClassName("pendants_menu")[0].classList.remove("slideLeft");
-          document.getElementsByClassName("items_7_menu")[0].classList.remove("slideLeft");
-          document.getElementsByClassName("industrial_menu")[0].classList.remove("slideLeft");
+        document.getElementsByClassName("lamps_menu")[0].classList.remove("slideLeft");
+        document.getElementsByClassName("pendants_menu")[0].classList.remove("slideLeft");
+        document.getElementsByClassName("item_7_menu")[0].classList.remove("slideLeft");
+        document.getElementsByClassName("industrial_menu")[0].classList.remove("slideLeft");
+    		myMenu[0].classList.remove('show');
     	}
   	}
 }
